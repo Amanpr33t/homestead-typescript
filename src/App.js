05dcac0705756103663cf0bc4d2afa512eb786ab
Amanpreet <aman11865@gmail.com>
2023-11-06 11:58:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import NavbarFieldAgent from "./components/field-agent/NavbarFieldAgent";
 import PropertiesAdded from "./components/field-agent/PropertiesAdded";
 import PropertyDealerAddForm from "./components/field-agent/PropertyDealerAddForm";
 import FieldAgentSignIn from "./components/field-agent/FieldAgentSignIn";
-import { Route, Routes, Navigate} from 'react-router-dom'
+import { Route, Routes, Navigate, Link } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <div className="box-border w-full min-h-screen">
+      <div className="fixed top-40">Hello</div>
       <NavbarFieldAgent />
       <Routes>
+        <Route path='/demo' element={<div className="top-40">Hello</div>}></Route>
         <Route path='/field-agent/*'>
           <Route path='' element={<HomeFieldAgent />}></Route>
           <Route path='signIn' element={<FieldAgentSignIn />}></Route>
